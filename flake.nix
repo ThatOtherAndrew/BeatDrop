0e552043e1fe844fa8a5bc346555f6450319644b
Andromeda <stroev.andrew@gmail.com>
2025-10-20 20:21:38 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "BeatDrop";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "beatdrop";
+        packages = with pkgs; [
+          bun
+        ];
+      };
+    };
+}
