da04e48fda54d4243e568e60e452ee45ef427c06
Andromeda <stroev.andrew@gmail.com>
2025-11-07 08:46:26 +0000

    Allow CF tunnel in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
     plugins: [sveltekit(), wasm(), devtoolsJson()],
+    server: {
+        allowedHosts: ['tunnel.thatother.dev'],
+    },
 });
